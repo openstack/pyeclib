436b176d50b2a0865bc17a39c72d9b4cf1eaf006
Kevin Greenan <kmgreen2@gmail.com>
2013-09-29 15:37:22 -0700

    Sample encoder and decoder for the Python library

---
+++ test/ec_pyeclib_decode.py
@@ -0,0 +1,47 @@
+import swift_ec_iface
+import swift_ec_iface.ec
+import swift_ec_iface.ec_pyeclib as ec_pyeclib
+from swift_ec_iface.ec import ECDriver
+import pyeclib
+import random
+import string
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Decoder for PyECLib.')
+parser.add_argument('k', type=int, help='number of data elements')
+parser.add_argument('m', type=int, help='number of parity elements')
+parser.add_argument('type', help='EC algorithm used')
+parser.add_argument('fragments', metavar='fragment', nargs='+',
+                   help='fragments to decode')
+parser.add_argument('filename', help='output file')
+
+args = parser.parse_args()
+
+print args.k
+print args.m
+print args.type
+print args.fragments
+print args.filename
+
+ec_driver = ECDriver("swift_ec_iface.ec_pyeclib.ECPyECLibDriver", k=args.k, m=args.m, type=args.type)
+
+fragment_list = []
+
+for fragment in args.fragments:
+  fp = open("%s" % fragment, "r")
+
+  fragment_list.append(fp.read())
+
+  fp.close()
+  
+
+fp = open("%s.decoded" % args.filename, "w")
+
+decoded_file=ec_driver.decode(fragment_list)
+ 
+fp.write(decoded_file)
+
+fp.close()
+
---
+++ test/ec_pyeclib_encode.py
@@ -0,0 +1,43 @@
+import swift_ec_iface
+import swift_ec_iface.ec
+import swift_ec_iface.ec_pyeclib as ec_pyeclib
+from swift_ec_iface.ec import ECDriver
+import pyeclib
+import random
+import string
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Encoder for PyECLib.')
+parser.add_argument('k', type=int, help='number of data elements')
+parser.add_argument('m', type=int, help='number of parity elements')
+parser.add_argument('type', help='EC algorithm used')
+parser.add_argument('file_dir', help='directory with the file')
+parser.add_argument('filename', help='file to encode')
+parser.add_argument('fragment_dir', help='directory to drop encoded fragments')
+
+args = parser.parse_args()
+
+print args.k
+print args.m
+print args.type
+print args.filename
+
+ec_driver = ECDriver("swift_ec_iface.ec_pyeclib.ECPyECLibDriver", k=args.k, m=args.m, type=args.type)
+
+fp = open("%s/%s" % (args.file_dir, args.filename), "r")
+ 
+whole_file_str = fp.read()
+   
+fragments=ec_driver.encode(whole_file_str)
+
+fp.close()
+
+i = 0
+for fragment in fragments:
+  fp = open("%s/%s.%d" % (args.fragment_dir, args.filename, i), "w")
+  fp.write(fragment)
+  fp.close()
+  i += 1
+  
