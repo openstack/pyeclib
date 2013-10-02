a8fad9c0e329c8d6b8e42b75fb66fb624e30f272
Kevin Greenan <kmg@box.com>
2013-10-01 17:26:27 -0700

    Added BSD license

--- src/c/xor_codes/test_xor_hd_code.h
+++ src/c/xor_codes/test_xor_hd_code.h
@@ -1,3 +1,27 @@
+/* * Copyright (c) 2013, Kevin Greenan (kmgreen2@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.  THIS SOFTWARE IS PROVIDED BY
+ * THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #define NUM_10_3_COMBS 55
 int failure_combs_10_3[NUM_10_3_COMBS][4] =  {{0, -1, -1, -1}, {1, -1, -1, -1}, {2, -1, -1, -1}, {3, -1, -1, -1}, {4, -1, -1, -1}, {5, -1, -1, -1}, {6, -1, -1, -1}, {7, -1, -1, -1}, {8, -1, -1, -1}, {9, -1, -1, -1}, {0, 1, -1, -1}, {0, 2, -1, -1}, {0, 3, -1, -1}, {0, 4, -1, -1}, {0, 5, -1, -1}, {0, 6, -1, -1}, {0, 7, -1, -1}, {0, 8, -1, -1}, {0, 9, -1, -1}, {1, 2, -1, -1}, {1, 3, -1, -1}, {1, 4, -1, -1}, {1, 5, -1, -1}, {1, 6, -1, -1}, {1, 7, -1, -1}, {1, 8, -1, -1}, {1, 9, -1, -1}, {2, 3, -1, -1}, {2, 4, -1, -1}, {2, 5, -1, -1}, {2, 6, -1, -1}, {2, 7, -1, -1}, {2, 8, -1, -1}, {2, 9, -1, -1}, {3, 4, -1, -1}, {3, 5, -1, -1}, {3, 6, -1, -1}, {3, 7, -1, -1}, {3, 8, -1, -1}, {3, 9, -1, -1}, {4, 5, -1, -1}, {4, 6, -1, -1}, {4, 7, -1, -1}, {4, 8, -1, -1}, {4, 9, -1, -1}, {5, 6, -1, -1}, {5, 7, -1, -1}, {5, 8, -1, -1}, {5, 9, -1, -1}, {6, 7, -1, -1}, {6, 8, -1, -1}, {6, 9, -1, -1}, {7, 8, -1, -1}, {7, 9, -1, -1}, {8, 9, -1, -1}} ;
 #define NUM_10_4_COMBS 175
--- src/c/xor_codes/tools/gen_goldilocks_codes.py
+++ src/c/xor_codes/tools/gen_goldilocks_codes.py
@@ -1,3 +1,27 @@
+
+# Copyright (c) 2013, Kevin Greenan (kmgreen2@gmail.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.  THIS SOFTWARE IS PROVIDED BY
+# THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import optparse
 import mpmath
 import itertools
@@ -158,18 +182,15 @@ while k > m:
 for parity_bm in used_parities:
   num_data_in_parity = get_num_data_in_parity(bm_parity_combinations)
   parity_eqns = get_parity_eqns(bm_parity_combinations) 
-  #print "(%d, %d) : %s : %s : %s" % (k, m, bm_parity_combinations, parity_eqns, num_data_in_parity)
-  print parity_bms_template % (k, m, num_bits + 1, ("%s" % parity_eqns).replace("[", "").replace("]", ""))
-  print data_bms_template % (k, m, num_bits + 1, ("%s" % bm_parity_combinations).replace("[", "").replace("]", ""))
+  print "(%d, %d) : %s : %s : %s" % (k, m, bm_parity_combinations, parity_eqns, num_data_in_parity)
+  #print parity_bms_template % (k, m, num_bits + 1, ("%s" % parity_eqns).replace("[", "").replace("]", ""))
+  #print data_bms_template % (k, m, num_bits + 1, ("%s" % bm_parity_combinations).replace("[", "").replace("]", ""))
   
   bm_parity_combinations.append(parity_bm)
   k += 1
   
 num_data_in_parity = get_num_data_in_parity(bm_parity_combinations)
 parity_eqns = get_parity_eqns(bm_parity_combinations) 
-#print "(%d, %d) : %s : %s" % (k, m, bm_parity_combinations, num_data_in_parity)
-print parity_bms_template % (k, m, num_bits + 1, ("%s" % parity_eqns).replace("[", "").replace("]", ""))
-print data_bms_template % (k, m, num_bits + 1, ("%s" % bm_parity_combinations).replace("[", "").replace("]", ""))
-
-
-
+print "(%d, %d) : %s : %s" % (k, m, bm_parity_combinations, num_data_in_parity)
+#print parity_bms_template % (k, m, num_bits + 1, ("%s" % parity_eqns).replace("[", "").replace("]", ""))
+#print data_bms_template % (k, m, num_bits + 1, ("%s" % bm_parity_combinations).replace("[", "").replace("]", ""))
