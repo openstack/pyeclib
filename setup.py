cf165e4e17ba1ffd790da12bedb1d6beb3225427
Kevin Greenan <kmgreen2@gmail.com>
2013-10-16 09:32:28 -0700

    More setup.py stuff with Pypi and pip...

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if found_jerasure is False:
 # umask set to 022 or something?
 #
 def _pre_build(dir):
-  ret = os.system('(cd c_eclib-0.2 && chmod 755 ./configure && ./configure && make install)')
+  ret = os.system('(cd c_eclib-0.2 && chmod 755 ./configure && chmod 755 ./install-sh && ./configure && make install)')
   if ret != 0:
     sys.exit(2)
 
@@ -51,7 +51,7 @@ module = Extension('pyeclib_c',
                    sources = ['src/c/pyeclib_c/pyeclib_c.c'])
 
 setup (name = 'PyECLib',
-       version = '0.1.8',
+       version = '0.1.9',
        author = 'Kevin Greenan',
        author_email = 'kmgreen2@gmail.com',
        maintainer = 'Kevin Greenan and Tushar Gohad',
