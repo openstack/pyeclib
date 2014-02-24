5e4313e8587be5e609a245c065c74d91e90c3db0
Tushar Gohad <tushar.gohad@intel.com>
2014-02-24 16:02:26 -0700

    Fix build_jerasure perms, Update MANIFEST

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except:
 # umask set to 022 or something?
 #
 def _pre_build(dir):
-  ret = os.system('(cd c_eclib-0.2 && ./build_jerasure.sh && chmod 755 ./configure && chmod 755 ./install-sh && ./configure %s && make install)' % autoconf_arguments)
+  ret = os.system('(cd c_eclib-0.2 && chmod 755 ./build_jerasure.sh && ./build_jerasure.sh && chmod 755 ./configure && chmod 755 ./install-sh && ./configure %s && make install)' % autoconf_arguments)
   if ret != 0:
     sys.exit(2)
 
