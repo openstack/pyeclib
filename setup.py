e26a510bf8540ae05a38361977e442faf3d8e063
Tushar Gohad <tushar.gohad@intel.com>
2014-08-05 08:35:45 -0700

    Update to v0.9.5
    Signed-off-by: Tushar Gohad <tushar.gohad@intel.com>

--- setup.py
+++ setup.py
@@ -207,7 +207,7 @@ module = Extension('pyeclib_c',
                    sources=['src/c/pyeclib_c/pyeclib_c.c'])
 
 setup(name='PyECLib',
-      version='0.9.4',
+      version='0.9.5',
       author='Kevin Greenan',
       author_email='kmgreen2@gmail.com',
       maintainer='Kevin Greenan and Tushar Gohad',
