45918310349556225f2125daf5b1f60ef3dee027
Tim Burke <tim.burke@gmail.com>
2016-12-07 20:14:22 +0000

    Install jerasure from source when testing liberasure-master
    Change-Id: I5e88c7268641b6bcad019748544ffd2694cee50f

--- tools/pip-install-with-liberasurecode.sh
+++ tools/pip-install-with-liberasurecode.sh
@@ -16,6 +16,34 @@ if [ -n "$ISAL_DIR" ]; then
     popd
 fi
 
+if [ -n "$GFCOMPLETE_DIR" ]; then
+    if [ ! -d "$GFCOMPLETE_DIR" ]; then
+        git clone http://lab.jerasure.org/jerasure/gf-complete.git "$GFCOMPLETE_DIR"
+    fi
+    pushd "$GFCOMPLETE_DIR"
+    ./autogen.sh
+    ./configure --prefix "$VIRTUAL_ENV"
+    make
+    make install
+    popd
+fi
+
+if [ -n "$JERASURE_DIR" ]; then
+    if [ -z "$GFCOMPLETE_DIR" ]; then
+        echo "JERASURE_DIR requires that GFCOMPLETE_DIR be set!"
+        exit 1
+    fi
+    if [ ! -d "$JERASURE_DIR" ]; then
+        git clone http://lab.jerasure.org/jerasure/jerasure.git "$JERASURE_DIR"
+    fi
+    pushd "$JERASURE_DIR"
+    autoreconf --force --install
+    LD_LIBRARY_PATH="$VIRTUAL_ENV"/lib ./configure --prefix "$VIRTUAL_ENV" LDFLAGS="-L$VIRTUAL_ENV/lib" CPPFLAGS="-I$VIRTUAL_ENV/include"
+    make
+    make install
+    popd
+fi
+
 if [ -z "$LIBERASURECODE_DIR" ]; then
     echo "Expected LIBERASURECODE_DIR to be set!"
     exit 1
