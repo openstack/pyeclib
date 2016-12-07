08bfdb2e48b67332a7468bb44fbb280a529a020c
Tim Burke <tim.burke@gmail.com>
2016-12-07 18:46:40 +0000

    Install isa-l from source when testing liberasure-master
    Change-Id: I93506b63f2a4f86358d5ffbca8b7360e797729d1

--- tools/pip-install-with-liberasurecode.sh
+++ tools/pip-install-with-liberasurecode.sh
@@ -3,6 +3,19 @@ if [ -z "$VIRTUAL_ENV" ]; then
     echo "Expected VIRTUAL_ENV to be set!"
     exit 1
 fi
+
+if [ -n "$ISAL_DIR" ]; then
+    if [ ! -d "$ISAL_DIR" ]; then
+        git clone git://github.com/01org/isa-l.git "$ISAL_DIR"
+    fi
+    pushd "$ISAL_DIR"
+    ./autogen.sh
+    ./configure --prefix "$VIRTUAL_ENV"
+    make
+    make install
+    popd
+fi
+
 if [ -z "$LIBERASURECODE_DIR" ]; then
     echo "Expected LIBERASURECODE_DIR to be set!"
     exit 1
@@ -20,4 +33,5 @@ fi
 make
 make install
 popd
+
 pip install "$@"
