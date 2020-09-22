2c10fdf2b3ad638105ebb07082959c22948096ca
Tim Burke <tim.burke@gmail.com>
2020-09-22 11:22:59 -0700

    Update gate jobs
    * Add py38 job
    * Add py36-on-centos8 job
    
    Change-Id: I020afad0bfe5144786bb8b6e1a99386f8c78a908

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -10,6 +10,11 @@ function is_rhel7 {
         cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
         cat /etc/*release | grep -q 'release 7'
 }
+function is_rhel8 {
+    [ -f /usr/bin/dnf ] && \
+        cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
+        cat /etc/*release | grep -q 'release 8'
+}
 
 
 if is_rhel7; then
@@ -19,4 +24,11 @@ if is_rhel7; then
     # Now that RDO repositories are enabled, install missing
     # packages.
     sudo yum install -y liberasurecode-devel yasm
+fi
+
+if is_rhel8; then
+    # Install CentOS OpenStack repos so that we have access to some extra
+    # packages.
+    sudo dnf install -y centos-release-openstack-ussuri
+    sudo dnf install -y liberasurecode-devel yasm
 fi
