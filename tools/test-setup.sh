8e71d37d318849127e1cc53fbd5ffec0b34119b7
Tim Burke <tim.burke@gmail.com>
2023-07-11 12:41:30 -0700

    Test on CentOS 9 Stream
    Change-Id: Icc4d156748f9cd5b64a2a37cec53f51b13f9a0eb

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -15,6 +15,11 @@ function is_rhel8 {
         cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
         cat /etc/*release | grep -q 'release 8'
 }
+function is_rhel9 {
+    [ -f /usr/bin/dnf ] && \
+        cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
+        cat /etc/*release | grep -q 'release 9'
+}
 
 
 if is_rhel7; then
@@ -31,4 +36,11 @@ if is_rhel8; then
     # packages.
     sudo dnf install -y centos-release-openstack-ussuri
     sudo dnf install -y liberasurecode-devel yasm
+fi
+
+if is_rhel9; then
+    # Install CentOS OpenStack repos so that we have access to some extra
+    # packages.
+    sudo dnf install -y centos-release-openstack-zed
+    sudo dnf install -y liberasurecode-devel nasm
 fi
