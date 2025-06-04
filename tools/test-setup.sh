f1e0aecda34811bfd6a171e8020b80e570dae1a8
Tim Burke <tim.burke@gmail.com>
2025-06-04 10:46:06 -0700

    Clean out some old cent7/cent8 setup
    We haven't supported those for a while.
    
    Related-Change: I24d653041df35304188500cbadfac8228bac5dce
    Related-Change: If00058451b89b93ed5b63ce4d6ade5f85785fc50
    Change-Id: I6676ace1c92f6d1be1a1c361e0fe60a5d6a17634

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -5,38 +5,12 @@
 
 # Install liberasurecode-devel for CentOS from RDO repository.
 
-function is_rhel7 {
-    [ -f /usr/bin/yum ] && \
-        cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
-        cat /etc/*release | grep -q 'release 7'
-}
-function is_rhel8 {
-    [ -f /usr/bin/dnf ] && \
-        cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
-        cat /etc/*release | grep -q 'release 8'
-}
 function is_rhel9 {
     [ -f /usr/bin/dnf ] && \
         cat /etc/*release | grep -q -e "Red Hat" -e "CentOS" -e "CloudLinux" && \
         cat /etc/*release | grep -q 'release 9'
 }
 
-
-if is_rhel7; then
-    # Install CentOS OpenStack repos so that we have access to some extra
-    # packages.
-    sudo yum install -y centos-release-openstack-train
-    # Now that RDO repositories are enabled, install missing
-    # packages.
-    sudo yum install -y liberasurecode-devel
-fi
-
-if is_rhel8; then
-    # Install CentOS OpenStack repos so that we have access to some extra
-    # packages.
-    sudo dnf install -y centos-release-openstack-ussuri
-    sudo dnf install -y liberasurecode-devel
-fi
 
 if is_rhel9; then
     # Install CentOS OpenStack repos so that we have access to some extra
